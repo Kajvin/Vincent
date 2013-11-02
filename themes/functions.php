b90f66dbecc77ea14b0b77889110ca4b231f8b8b
Kajvin <sm7oea@msn.com>
2013-11-02 14:56:55 +0100

    Adding content-editior and controller and model for Content, CCBlog, CCPage, filtering of content as plain, html and php, bbcode foramting capabilty, and support to format & filter conten using HTMLPurifier

--- themes/functions.php
+++ themes/functions.php
@@ -67,7 +67,7 @@ function get_messages_from_session() {
 function login_menu() {
   $wi = CVincent::Instance();
   if($wi->user['isAuthenticated']) {
-     $items = "<a href='" . create_url('user/profile') . "'><img class='gravatar' src='" . get_gravatar(20) . "' alt=''> " . $wi->user['acronym'] . "</a> ";
+    $items = "<a href='" . create_url('user/profile') . "'><img class='gravatar' src='" . get_gravatar(20) . "' alt=''> " . $wi->user['acronym'] . "</a> ";
     if($wi->user['hasRoleAdministrator']) {
       $items .= "<a href='" . create_url('acp') . "'>acp</a> ";
     }
@@ -75,15 +75,49 @@ function login_menu() {
   } else {
     $items = "<a href='" . create_url('user/login') . "'>login</a> ";
   }
-  return "<nav>$items</nav>";
+  return "<nav id='login-menu'>$items</nav>";
 }
 
 
 /**
-* Get a gravatar based on the user's email.
-*/
+ * Get a gravatar based on the user's email.
+ */
 function get_gravatar($size=null) {
-  return 'http://www.gravatar.com/avatar/' . md5(strtolower(trim(CVincent::Instance()->user['email']))) . '.jpg?' . ($size ? "s=$size" : null);
+  return 'http://www.gravatar.com/avatar/' . md5(strtolower(trim(CVincent::Instance()->user['email']))) . '.jpg?r=pg&amp;d=wavatar&amp;' . ($size ? "s=$size" : null);
+}
+
+
+/**
+ * Escape data to make it safe to write in the browser.
+ *
+ * @param $str string to escape.
+ * @returns string the escaped string.
+ */
+function esc($str) {
+  return htmlEnt($str);
+}
+
+
+/**
+ * Filter data according to a filter. Uses CMContent::Filter()
+ *
+ * @param $data string the data-string to filter.
+ * @param $filter string the filter to use.
+ * @returns string the filtered string.
+ */
+function filter_data($data, $filter) {
+  return CMContent::Filter($data, $filter);
+}
+
+
+/**
+ * Display diff of time between now and a datetime. 
+ *
+ * @param $start datetime|string
+ * @returns string
+ */
+function time_diff($start) {
+  return formatDateTimeDiff($start);
 }
 
 
